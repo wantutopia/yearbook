877100dd104ff8fc57415b8c32aea744648312bf
김병규/티켓링크개발TF <kim.bk@nhnent.com>
2016-06-08 09:53:51 +0900

    서버 기본 셋팅

--- src/main/java/com/yearbook/controller/HelloController.java
+++ src/main/java/com/yearbook/controller/HelloController.java
@@ -1,16 +1,25 @@
-package com.yearbook;
+package com.yearbook.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.Properties;
+
 @Controller
 @RequestMapping("/")
 public class HelloController {
+
+	@Autowired
+	@Qualifier(value="commonConfig")
+	Properties commonConfig;
+
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
-		model.addAttribute("message", "Hello world!");
+		model.addAttribute("message", commonConfig.getProperty("hello"));
 		return "hello";
 	}
 }
